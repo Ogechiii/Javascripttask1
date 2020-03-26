1cd3ed39cc1e95ea4dc98256c23f79d051b4a454
ogechiii <okerekeemmanuel007@gmail.com>
2020-03-26 05:02:20 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let myName = 'Okereke Emmanuel';
+
+const courses = ['Html', 'Css', 'Javascript']
+console.log(myName);
+console.log(courses);
+
+for(let i=1; i<=200; i++){
+if (i % 2 !==0)
+console.log(i);
+}
+
+
